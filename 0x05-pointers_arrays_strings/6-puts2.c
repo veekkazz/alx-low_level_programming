c72e11184192b9112431855a618f91abbac75bf9
veekkazz <vickosemi01@gmail.com>
2023-05-03 22:01:56 -0700

    prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,25 +1,29 @@
 #include "main.h"
-
 /**
- * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-
-void rev_string(char *s)
+void puts2(char *str)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
+	while (*y != '\0')
 	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		y++;
+		longi++;
 	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
 }
-
