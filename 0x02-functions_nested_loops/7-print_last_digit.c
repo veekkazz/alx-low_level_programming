7c8a79facaccbd113cea0de62e79568ce17b28f0
veekkazz <vickosemi01@gmail.com>
2023-05-04 06:01:31 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,16 @@
  */
 int print_last_digit(int n)
 {
-	int l;
+	int a;
 
-	l = n % 10;
-	if (l < 0)
-	{
-		_putchar(-l + 48);
-		return (-l);
-	}
-	else
-	{
-		_putchar(l + 48);
-		return (l);
-	}
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+
+	return (a);
 }
