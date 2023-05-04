77e4d34c8c879022eb6eea34f4e72cd87e310008
veekkazz <vickosemi01@gmail.com>
2023-05-04 07:51:40 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper.c - Check if a letter is upper.
+ * _isupper - check for upper case letter
  * @x: The number to be checked.
  * Description: main - check the code.
  * Return: 1 for upper or 0 for any else.
