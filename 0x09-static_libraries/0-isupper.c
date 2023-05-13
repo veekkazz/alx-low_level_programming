4be2e9fb5dc27ae8f4f1470f73b405b06458fe06
veekkazz <vickosemi01@gmail.com>
2023-05-13 03:28:55 -0700

    is upper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case letter
+ * @x: The number to be checked.
+ * Description: main - check the code.
+ * Return: 1 for upper or 0 for any else.
+ */
+
+int _isupper(int x)
+
+{
+	return (x >= 65 && x <= 90);
+}
